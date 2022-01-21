85fa1ae8a68842f6f4c80be11c60bd5d672e264c
Caleb Knight <knightca@oregonstate.edu>
2022-01-21 11:11:59 -0800

    Refactor into 3 functions that run the shell, then run shell is the only thing in main()

--- mush.c
+++ mush.c
@@ -8,55 +8,65 @@
 #include <unistd.h> //fork, execvp
 #include <sys/wait.h> //wait
 
-int run_shell()
+char input[2048];
+char *new_argv[128];
+int i = 0;
+char *token;
+
+
+void user_interface()
 {
-	char input[2048];
-	char *new_argv[128];
+	printf("-ksh$ ");
+	fflush(stdout); //Assure prompt display
+	fgets(input, sizeof input, stdin); //Read line from stdin
+	printf("%s\n", input);
+}
 
-	while(1)
-	{
-		int i = 0;
-		char *token;
+void parse()
+{
+	if ((token = strtok(input, " \t\n\r")) != NULL) {
+		do {
+			new_argv[i++] = token;
+			printf("\"%s\"\n", token);
+		} while ((token = strtok(NULL, " \t\n\r")) != NULL);
+	}
+	new_argv[i] = NULL;
 
-		printf("-ksh$ ");
-		fflush(stdout); //Assure prompt display
-		fgets(input, sizeof input, stdin); //Read line from stdin
-		printf("%s\n", input);
+}
 
-		if ((token = strtok(input, " \t\n\r")) != NULL) {
-			do {
-				new_argv[i++] = token;
-				printf("\"%s\"\n", token);
-			} while ((token = strtok(NULL, " \t\n\r")) != NULL);
+void command_handler() 
+{
+	int cd_check;
+
+	if(strcmp(new_argv[0], "cd") == 0) {
+		cd_check = chdir(new_argv[1]);
+		if(cd_check == -1)
+			perror("Error");
+	} 
+	else if(strcmp(new_argv[0], "exit") == 0)
+		exit(0);
+	else {
+		pid_t pid = fork();
+
+		if(pid == 0) {
+			execvp(new_argv[0], new_argv);
+			perror("exec");
+			exit(1);
 		}
-		new_argv[i] = NULL;
-
-		int cd_check;
-		if(strcmp(new_argv[0], "cd") == 0) {
-			cd_check = chdir(new_argv[1]);
-			if(cd_check == -1)
-				perror("Error");
-		} 
-		else if(strcmp(new_argv[0], "exit") == 0)
-			exit(0);
 		else {
-			pid_t pid = fork();
-
-			if(pid == 0) {
-				execvp(new_argv[0], new_argv);
-				perror("exec");
-				exit(1);
-			}
-			else {
-				wait(NULL);
-			}
+			wait(NULL);
 		}
 	}
 }
 
-int command_handler() 
+int run_shell()
 {
-	return 0;
+	while(1)
+	{
+		user_interface();
+		parse();
+		command_handler();
+	}
 }
 
 int main(int argc, char *argv[])
